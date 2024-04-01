3ab5be9c31b137186a1cc1a8695d84561a78c088
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-04-01 23:43:05 +0300

    added animation

--- src/app/Components/AboutBlock/AboutBlock.js
+++ src/app/Components/AboutBlock/AboutBlock.js
@@ -1,21 +1,56 @@
+"use client";
+import { useState, useLayoutEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from "@/app/Components/AboutBlock/aboutBlock.module.scss";
 import Button from "@/app/Components/Button/Button";
 import Title from "@/app/Components/Title/Title";
 
 export default function AboutBlock() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    const tlImg1 = gsap.timeline({
+      scrollTrigger: { trigger: ".animation-img1", scrub: true, restart: true },
+    });
+    tlImg1.to(".animation-img1", { scale: 1 });
+
+    const tlImg2 = gsap.timeline({
+      scrollTrigger: { trigger: ".animation-img2", scrub: true, restart: true },
+    });
+    tlImg2.to(".animation-img2", { scale: 1 });
+
+    const tlImg3 = gsap.timeline({
+      scrollTrigger: { trigger: ".animation-img3", scrub: true, restart: true },
+    });
+    tlImg3.to(".animation-img3", { scale: 1 });
+  }, []);
+
   return (
     <div className={styles.about}>
       <div className={styles.about__gallery}>
         <div>
-          <img src="./image/image_2.png" alt="architecture"></img>
-          <img src="./image/image_4.png" alt="architecture"></img>
+          <img
+            className="animation-img1"
+            src="./image/image_2.png"
+            alt="architecture"
+          ></img>
+          <img
+            className="animation-img2"
+            src="./image/image_4.png"
+            alt="architecture"
+          ></img>
         </div>
         <div>
-          <img src="./image/image_3.png" alt="architecture"></img>
+          <img
+            className="animation-img3"
+            src="./image/image_3.png"
+            alt="architecture"
+          ></img>
         </div>
       </div>
       <div className={styles.about__text}>
-        <Title>О компании</Title>
+        <Title className>О компании</Title>
         <div>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
